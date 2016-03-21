a6a69ed18bc2016d4ae23888b9087cce6e73deeb
Pawlo1501 <pawlo1501@gmail.com>
2016-03-21 21:03:28 +0100

    walidacja usług

--- app/Model/Service.php
+++ app/Model/Service.php
@@ -1,5 +1,40 @@
 <?php
 App::uses('AppModel', 'Model');
+
 class Service extends AppModel {
 
+    public $useTable = 'services';
+
+    public $validate = array(
+        'service_name' => array(
+            'notBlank' => array(
+                'rule' => array('notBlank'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or
+            ),
+        ),
+        'service_time' => array(
+            'notBlank' => array(
+                'rule' => array('notBlank'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or
+            ),
+        ),
+        'price' => array(
+            'notBlank' => array(
+                'rule' => array('notBlank'),
+                //'message' => 'Your custom message here',
+                //'allowEmpty' => false,
+                //'required' => false,
+                //'last' => false, // Stop validation after this rule
+                //'on' => 'create', // Limit validation to 'create' or
+            ),
+        ),
+    );
 }
